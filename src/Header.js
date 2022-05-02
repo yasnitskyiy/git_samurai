bdf5c5fd30db8585d6758ee40861a19b4c4b8ec3
yasnitskyiy <yura.yasnitskiy@gmail.com>
2022-05-02 14:05:44 +0300

    send to git

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+
+const Header = () =>{
+    return (
+        <div>
+            <a href="#s">HOME</a>
+            <a href="#s">NEWS</a>
+            <a href="#s">ABOUT</a>
+        </div>
+    );
+}
+
+export default Header;
